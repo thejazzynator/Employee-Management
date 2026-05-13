348f63dbdf705d9093434f8dd251e91448b7cc6d
Jasmine Graham <thejazzynator@gmail.com>
2026-05-12 21:37:37 -0400

    crud operations

--- Employee Management/Controllers/EmployeeController.cs
+++ Employee Management/Controllers/EmployeeController.cs
@@ -24,11 +24,40 @@ namespace Employee_Management.Controllers
             return Ok(allemploees);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _employeeRepository.GetEmployeeByIdAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
         [HttpPost]
         public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
         {
             await _employeeRepository.AddEmployeeAsync(employee);
-            return Created();
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployeeById(int id)
+        {
+            await _employeeRepository.DeleteEmployeeAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> UpdateEmployeeAsync(int id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+            await _employeeRepository.UpdateEmployeeAsync(employee);
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
         }
     }
 }
